5961799e5078fd78fc33b3fb017673be292a9745
tekihei2317 <tekihei2317@gmail.com>
2022-05-12 20:02:19 +0900

    refactor: コンポーネントに書いていたゲームのロジックを、コンポーネント外に移動

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,31 +1,6 @@
 import { useState } from "react";
 import { Square } from "./Square";
-
-type Player = "X" | "O";
-
-function calculateNextPlayer(player: Player) {
-  return player === "X" ? "O" : "X";
-}
-
-function calculateWinner(squares: (string | null)[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+import { Player, calculateNextPlayer, calculateWinner } from "../utils/game";
 
 export const Board: React.FC = () => {
   type BoardState = {
