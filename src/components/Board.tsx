c661b99da9224e2841410c2c37bcec9fb31a1ac3
tekihei2317 <tekihei2317@gmail.com>
2022-05-12 19:55:20 +0900

    feat: 勝者の計算処理を実装 & すでに置いた場所に置けないように修正

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,6 +7,26 @@ function calculateNextPlayer(player: Player) {
   return player === "X" ? "O" : "X";
 }
 
+function calculateWinner(squares: (string | null)[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export const Board: React.FC = () => {
   type BoardState = {
     squares: (string | null)[];
@@ -17,7 +37,17 @@ export const Board: React.FC = () => {
     nextPlayer: "X",
   });
 
+  // winnerはなぜ再計算されるのだろうか？
+  const winner = calculateWinner(state.squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next Player: ${state.nextPlayer}`;
+
   const handleClick = (i: number) => {
+    if (calculateWinner(state.squares) || state.squares[i]) {
+      return;
+    }
+
     const squares = state.squares.slice();
     squares[i] = state.nextPlayer;
     setState({ squares, nextPlayer: calculateNextPlayer(state.nextPlayer) });
@@ -29,7 +59,7 @@ export const Board: React.FC = () => {
 
   return (
     <div>
-      <div className="status">Next Player: {state.nextPlayer}</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
